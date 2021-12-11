9a333d2e4697818bd3e629d4c42de2727d4ed883
DangeRSouL98 <wellsonleewando0908@gmail.com>
2021-12-11 15:02:31 +0700

    Fix asset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <div style="max-width: 100%; display: flex; flex-wrap: wrap; justify-content: space-around;">
         @foreach($contents as $content)
             <div class="card" style="width: 18rem; margin: 10px 0;">
-                <img class="card-img-top" src="assets/test.jpg" alt="Card image cap">
+                <img class="card-img-top" src="{{ asset('assets/test.jpg') }}" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">{{ $content->title }}</h5>
                     <h6>By: {{ $content->author }} published at: {{ $content->published_at }}</h6>
