57407443f22449d47346020519e97c25b60110a1
vyxtris <test>
2021-12-12 23:10:54 +0700

    Style - navbar login register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,24 +11,28 @@
 @if(session()->has('error'))
     <small>{{ session('error') }}</small>
 @endif
-<form action="/login" method="post">
-    @csrf
-    <div class="my-3 mx-3 row">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-        <div class="col-sm-5">
-            <input type="text" class="form-control" name="email" id="email" value="">
+    <div class="login-content">
+        <div class="form-container">
+            <form action="/login" method="post">
+                @csrf
+                <div class="my-3 mx-3 row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-5">
+                        <input type="text" class="form-control" name="email" id="email" value="">
+                    </div>
+                </div>
+                <div class="my-3 mx-3 row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-5">
+                        <input type="password" class="form-control" name="password" id="password">
+                    </div>
+                </div>
+                <div class="my-3 mx-3 row col-sm-1">
+                    <button type="submit" class="btn btn-primary mb-3">Login</button>
+                </div>
+            </form>
         </div>
+        <small class="invite-text">Don't have an account? <a href="/register">Register</a> now!!</small>
     </div>
-    <div class="my-3 mx-3 row">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-        <div class="col-sm-5">
-            <input type="password" class="form-control" name="password" id="password">
-        </div>
-    </div>
-    <div class="my-3 mx-3 row col-sm-1">
-        <button type="submit" class="btn btn-primary mb-3">Login</button>
-    </div>
-</form>
-<small>Don't have an account? <a href="/register">Register</a> now!!</small>
 @endsection
-
+    
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,45 +5,49 @@
 @endsection
 
 @section('container')
-<form action="/register" method="post">
-    @csrf
-    <div class="my-3 mx-3 row">
-        <label for="name" class="col-sm-2 col-form-label">Name</label>
-        <div class="col-sm-5">
-            <input type="text" class="form-control" name="name" id="name" value="">
+<div class="login-content">
+    <div class="form-container">
+        <form action="/register" method="post">
+            @csrf
+            <div class="my-3 mx-3 row">
+            <label for="name" class="col-sm-2 col-form-label">Name</label>
+            <div class="col-sm-5">
+                <input type="text" class="form-control" name="name" id="name" value="">
+            </div>
+            @error('name')
+            <div class="col-sm-5">
+                {{ $message }}
+            </div>
+            @enderror
         </div>
-        @error('name')
-        <div class="col-sm-5">
-            {{ $message }}
+        <div class="my-3 mx-3 row">
+            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-5">
+                <input type="email" class="form-control" name="email" id="email" value="">
+            </div>
+            @error('email')
+            <div class="col-sm-5">
+                {{ $message }}
+            </div>
+            @enderror
         </div>
-        @enderror
-    </div>
-    <div class="my-3 mx-3 row">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-        <div class="col-sm-5">
-            <input type="email" class="form-control" name="email" id="email" value="">
+        <div class="my-3 mx-3 row">
+            <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-5">
+                <input type="password" class="form-control" name="password" id="inputPassword">
+            </div>
+            @error('password')
+            <div class="col-sm-5">
+                {{ $message }}
+            </div>
+            @enderror
         </div>
-        @error('email')
-        <div class="col-sm-5">
-            {{ $message }}
+        <div class="my-3 mx-3 row col-sm-1">
+            <button type="submit" class="btn btn-primary mb-3">Register</button>
         </div>
-        @enderror
-    </div>
-    <div class="my-3 mx-3 row">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-        <div class="col-sm-5">
-            <input type="password" class="form-control" name="password" id="inputPassword">
-        </div>
-        @error('password')
-        <div class="col-sm-5">
-            {{ $message }}
-        </div>
-        @enderror
-    </div>
-    <div class="my-3 mx-3 row col-sm-1">
-        <button type="submit" class="btn btn-primary mb-3">Register</button>
-    </div>
-</form>
-<small>Already have an account? <a href="/login">Login</a> here!!</small>
+    </form>
+</div>
+    <small class="invite-text">Already have an account? <a href="/login">Login</a> here!!</small>
+</div>
 @endsection
 
