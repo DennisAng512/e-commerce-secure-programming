e1a60b9ceab0196d75e2ee1761ed5849d74b4b55
vyxtris <test>
2021-12-20 10:50:44 +0700

    Update books query

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -18,7 +18,7 @@ class BookController extends Controller
 
         return view('index', [
             "title" => $title,
-            "contents" => Book::latest()->search(request(['search','tags']))->paginate(5)->withQueryString()
+            "contents" => Book::latest()->search(request(['search','tags']))->where('is_deleted', '=', '0')->paginate(5)->withQueryString()
         ]);
     }
 
@@ -27,5 +27,9 @@ class BookController extends Controller
             "title" => "Book",
             "content" => $book
         ]);
+    }
+
+    public function delete(Book $book){
+
     }
 }
