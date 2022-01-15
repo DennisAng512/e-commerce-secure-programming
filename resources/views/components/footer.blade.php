9c194760b7f01b2a3e70104213098430cc78f2a1
vyxtris <test>
2022-01-16 02:09:48 +0700

    Remove some footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,29 +1,15 @@
 <br>
-<footer class="footer spad">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="footer__about">
-                    <div class="footer__about__logo">
-                        <a href="./index.html"><img src="{{ asset('assets/logo.jpg') }}" alt=""></a>
-                    </div>
-                    <ul>
-                        <li>Address: Kebon Jeruk, Jakarta Barat</li>
-                        <li>Phone: +62 812 9023 5876</li>
-                        <li>Email: mh@manga.com</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-12">
-                <div class="footer__widget">
-                    <h6>Join Our Newsletter Now</h6>
-                    <p>Get E-mail updates about our latest shop and special offers.</p>
-                    <form action="#">
-                        <input type="text" placeholder="Enter your mail">
-                        <button type="submit" class="site-btn">Subscribe</button>
-                    </form>
-                </div>
-            </div>
+<footer class="footer">
+    <div class="footer-row">
+        <div class="footer-c logo w-50 m-0">
+            <a href="./index.html"><img src="{{ asset('assets/logo.jpg') }}" alt="">logo</a>
+        </div>
+        <div class="footer-c">
+            <ul class="footer-list">
+                <li>Address: Kebon Jeruk, Jakarta Barat</li>
+                <li>Phone: +62 812 9023 5876</li>
+                <li>Email: mh@manga.com</li>
+            </ul>
         </div>
     </div>
 </footer>
